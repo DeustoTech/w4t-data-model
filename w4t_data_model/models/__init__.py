9b351116ac52d124b78274709e524cadd4c6a77e
josu.bermudez <josu.bermudez@deusto.es>
2019-03-28 14:05:53 +0100

    treatment typo fixed

--- w4t_data_model/models/__init__.py
+++ w4t_data_model/models/__init__.py
@@ -1,5 +1,5 @@
 from w4t_data_model.models.collection_points import DepositPoint, DepositPointType, DepositPointIsle
 from w4t_data_model.models.monitoring import KeyPerformanceIndicator
-from w4t_data_model.models.treatament import TreatmentPlantType, TreatmentPlant
+from w4t_data_model.models.treatment import TreatmentPlantType, TreatmentPlant
 from w4t_data_model.models.waste_characterizations import SortingType, ResourceCategory, Resource
 from w4t_data_model.models.social import Action
